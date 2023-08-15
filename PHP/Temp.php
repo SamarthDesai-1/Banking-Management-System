24660f2a78b8f49cb8e884c80540e89dae27467c
sdesaih <samarthdesain@gmail.com>
2023-08-15 17:57:58 +0530

    Commit Success

---
+++ PHP/Temp.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .container {
+            max-width: 1180px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ex nulla at voluptas quam voluptatem, vel est atque nostrum veniam nihil accusantium! Praesentium recusandae est ex pariatur expedita explicabo mollitia?
+    </div>
+</body>
+</html>
